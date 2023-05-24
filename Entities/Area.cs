3da70d2c92c57f2eaf2dbbbfbcd2965486c98634
mondayscool <76455195+mondayscool@users.noreply.github.com>
2023-05-23 19:52:02 -0700

    add constraints for columns, areaid property to sectors

--- Entities/Area.cs
+++ Entities/Area.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,12 +9,16 @@ namespace PitchLogLib.Entities
 {
     public class Area : EntityBase
     {
+        [MaxLength(256)]
         public string Name { get; set; } = string.Empty;
 
+        [MaxLength(256)]
         public string Municipality { get; set; } = string.Empty;
 
+        [MaxLength(2)]
         public string Region { get; set; } = string.Empty;
 
+        [MaxLength(2)]
         public string Country { get; set; } = string.Empty;
 
         public IList<Sector> Sectors { get; set; } = new List<Sector>();
